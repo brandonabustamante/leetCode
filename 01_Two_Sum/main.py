90bae35c8a9d551a9e728a875c05de34973a61e2
Brandon A Bustamante <brandonabus1995@gmail.com>
2022-10-18 21:51:17 -0500

    unoptimized solution

--- 01_Two_Sum/main.py
+++ 01_Two_Sum/main.py
@@ -1,2 +1,25 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+
+def twoSum(nums, target):
+    
+    indexes = []
+
+    for i in range(0, len(nums)):
+        cur_val = target - nums[i]
+        for j in range(i + 1, len(nums)):
+            if cur_val - nums[j] == 0:
+                indexes.append(i)
+                indexes.append(j)
+
+    return indexes
+
+
+
+
+
+
+
+nums = [2, 7, 11, 15]
+target = 9
+
+print(twoSum(nums, 9))
