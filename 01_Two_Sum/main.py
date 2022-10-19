0f718f7b14173f8f582bee2874834b35cd4fe402
Brandon A Bustamante <brandonabus1995@gmail.com>
2022-10-19 11:45:11 -0500

    optimized solution

--- 01_Two_Sum/main.py
+++ 01_Two_Sum/main.py
@@ -2,21 +2,25 @@
 
 def twoSum(nums, target):
     
-    indexes = []
+    
+    # indexes = []
 
-    for i in range(0, len(nums)):
-        cur_val = target - nums[i]
-        for j in range(i + 1, len(nums)):
-            if cur_val - nums[j] == 0:
-                indexes.append(i)
-                indexes.append(j)
+    # for i in range(0, len(nums)):
+    #     cur_val = target - nums[i]
+    #     for j in range(i + 1, len(nums)):
+    #         if cur_val - nums[j] == 0:
+    #             indexes.append(i)
+    #             indexes.append(j)
 
-    return indexes
+    # return indexes
 
+    num_values = {}
 
-
-
-
+    for index, value in enumerate(nums):
+        if target - value in num_values:
+            return [num_values[target - value], index]
+        else:
+            num_values[value] = index
 
 
 nums = [2, 7, 11, 15]
